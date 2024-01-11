5a06b2b99279c38aedc73853ce5f4aeb8a2c061b
NupeCoder <NupeCoder@gmail.com>
2024-01-11 06:26:51 +0100

    Finally deployment

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,3 +1,18 @@
 import axios from 'axios';
 
-const fetchWeather = async () => {};
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'a6dd6828e548caa134845eaeab4f5eaa';
+
+const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    },
+  });
+
+  return data;
+};
+
+export { fetchWeather };
